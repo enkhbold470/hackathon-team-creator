61c072afe4647b5238aa77ba05c4596f6aa4a86b
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-05-02 18:37:33 -0700

    Create login page

---
+++ app/login/page.tsx
@@ -0,0 +1,24 @@
+import { LoginForm } from "@/components/login-form/login-form";
+import { loggedIn } from "@/lib/utils";
+import { redirect } from "next/navigation";
+
+export default function LoginPage() {
+  if (loggedIn) {
+    redirect("/dashboard");
+  }
+
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-background to-muted p-4">
+      <div className="w-full max-w-md">
+        <div className="mb-8 flex flex-col items-center text-center">
+          <h1 className="text-3xl font-bold">DA Hacks 3.5</h1>
+
+          <p className="mt-2 text-muted-foreground">
+            find your hackathon team!
+          </p>
+        </div>
+        <LoginForm />
+      </div>
+    </div>
+  );
+}

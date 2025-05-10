7339ced4f944bf9457c7dd25a87155f16d64d147
enkhbold470 <enkhbold470@gmail.com>
2025-05-10 03:00:15 -0700

    Update redirect from discover to matches page and remove obsolete discover page component

--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,5 @@ import { redirect } from "next/navigation"
 export default function Home() {
   // You can implement your auth check here later
   // For now, we'll just redirect to the discover page
-  return redirect("/discover")
+  return redirect("/matches")
 }

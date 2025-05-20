209f375dca4ca525305fed1dde61fc3f08dfab66
enkhbold470 <enkhbold470@gmail.com>
2025-05-20 06:53:29 -0700

    Enhance ProfilePage and CustomUserButton components to display current user information. Implement fetching of the current user in ProfilePage and update the user button to show the logged-in user's email address. Improve user experience by providing immediate feedback on user status.

--- components/customUserButton.tsx
+++ components/customUserButton.tsx
@@ -4,12 +4,23 @@ import Link from "next/link";
 export async function CustomUserButton() {
   const user = await getCurrentUser();
   return (
+    <>
+    <div className="flex items-center gap-2">
+    <div className="text-xs opacity-50">
+      <p>Logged in as</p>
+      <p>{user?.emailAddresses[0]?.emailAddress}</p>
+    </div>
     <Link href="/profile">
     <div className="rounded-full bg-primary text-primary-foreground w-12 h-12 flex items-center justify-center">
-            {user?.firstName?.charAt(0)}
-            {user?.lastName?.charAt(0)}
+            {/* {user?.firstName?.charAt(0)}
+            {user?.lastName?.charAt(0)} */}
+            {user?.emailAddresses[0]?.emailAddress?.slice(0, 2)}
+            
+
     </div>
     </Link>
+    </div>
+    </>
   );
 }
 

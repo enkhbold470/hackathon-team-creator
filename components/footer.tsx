72fc9cf2fb6e62dd08ca22dfc1eab8abe75103d9
enkhbold470 <enkhbold470@gmail.com>
2025-05-22 19:56:52 -0700

    Update footer component to change GitHub Issues link to the correct repository.

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
   return (
     <footer className="py-2 bottom-0 w-full text-sm border-t justify-center flex flex-col items-center">
         <p>You notice bug? or something is not working?</p>
-        <p>Report it on <Link href="https://github.com/Bongs237/hackathon-team-creator/issues"  target="_blank" className="underline" >Github Issues</Link></p>
+        <p>Report it on <Link href="https://github.com/enkhbold470/hackathon-team-creator/issues"  target="_blank" className="underline" >Github Issues</Link></p>
     </footer>
   );
 }

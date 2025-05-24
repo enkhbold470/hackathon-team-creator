53c74804e28e79ef9d37cfa656628d212acc9188
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-05-24 01:44:52 -0700

    Correct grammar in the footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,8 +3,17 @@ import Link from "next/link";
 export function Footer() {
   return (
     <footer className="py-2 bottom-0 w-full text-sm border-t justify-center flex flex-col items-center">
-        <p>You notice bug? or something is not working?</p>
-        <p>Report it on <Link href="https://github.com/enkhbold470/hackathon-team-creator/issues"  target="_blank" className="underline" >Github Issues</Link></p>
+      <span>
+        See a bug? Create an{" "}
+        <Link
+          href="https://github.com/enkhbold470/hackathon-team-creator/issues"
+          target="_blank"
+          className="underline"
+        >
+          issue
+        </Link>{" "}
+        on Github.
+      </span>
     </footer>
   );
 }
